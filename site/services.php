e7f7c98c57e275b1f29639cf8cd1e539505ac171
Alex Valov <vlolipops@gmail.com>
2013-03-30 02:20:35 +0300

    don't now

--- site/services.php
+++ site/services.php
@@ -17,38 +17,38 @@
 		<tr valign="top">
 			<td id="contentcss">
 				<h3 onclick="expandcontent('sc1')" style="cursor:hand; cursor:pointer">Молитвенное</h3>
-				<div id="sc1" class="switchcontent">Куга Любовь +7(921)317-55-96 </div>
+				<div id="sc1" class="switchcontent"> Руководитель Молитвенного служения - Куга Любовь +7(921)317-55-96 </div>
 
 				<h3 onclick="expandcontent('sc2')" style="cursor:hand; cursor:pointer">Библейские курсы</h3>
-				<div id="sc2" class="switchcontent">Дмитриев Юрий (декан) +7(911)000-21-61</div>
+				<div id="sc2" class="switchcontent"> Декан библейских курсов - Дмитриев Юрий (декан) +7(911)000-21-61</div>
 				
 				<h3 onclick="expandcontent('sc3')" style="cursor:hand; cursor:pointer">Прославление</h3>
-				<div id="sc3" class="switchcontent"> Федосеева Анна +7(921)314-47-33</div>
+				<div id="sc3" class="switchcontent"> Руководитель служения Прославления - Федосеева Анна +7(921)314-47-33</div>
 
 				<h3 onclick="expandcontent('sc4')" style="cursor:hand; cursor:pointer">Видеослужение</h3>
-				<div id="sc4" class="switchcontent"> Алексей Черноволов +7(911)242-90-48</div>				
+				<div id="sc4" class="switchcontent"> Руководитель Видеослужения - Алексей Черноволов +7(911)242-90-48</div>				
 
 				<h3 onclick="expandcontent('sc5')" style="cursor:hand; cursor:pointer">Евангелизационное</h3>
-				<div id="sc5" class="switchcontent"> Ерoшков Сергей +7(952)222-98-28 </div>
+				<div id="sc5" class="switchcontent"> Руководитель Евангелизационного служения - Ерoшков Сергей +7(952)222-98-28 </div>
 
 				<h3 onclick="expandcontent('sc6')" style="cursor:hand; cursor:pointer">Миссионерское</h3>
-				<div id="sc6" class="switchcontent"> Богданов Игорь +7(951)660-02-36</div>				
+				<div id="sc6" class="switchcontent"> Руководитель Миссионерского служения - Богданов Игорь +7(951)660-02-36</div>				
 
 				<h3 onclick="expandcontent('sc7')" style="cursor:hand; cursor:pointer">Тюремное</h3>
-				<div id="sc7" class="switchcontent"> Георгиева Ирина +7(904)619-27-01</div>
+				<div id="sc7" class="switchcontent"> Руководитель Тюремного служения - Георгиева Ирина +7(904)619-27-01</div>
 
 				<h3 onclick="expandcontent('sc8')" style="cursor:hand; cursor:pointer">Водное крещение</h3>
-				<div id="sc8" class="switchcontent"> Парийская Людмила +7(950)012-08-53</div>				
+				<div id="sc8" class="switchcontent"> Руководитель служения Водного крещения - Парийская Людмила +7(950)012-08-53</div>				
 
 				<h3 onclick="expandcontent('sc9')" style="cursor:hand; cursor:pointer">Молодёжное</h3>
-				<div id="sc9" class="switchcontent"> Богданов Даниил +7(953)345-39-18 </div>
+				<div id="sc9" class="switchcontent"> Руководитель Молодёжного служения - Богданов Даниил +7(953)345-39-18 </div>
 
 				<h3 onclick="expandcontent('sc10')" style="cursor:hand; cursor:pointer">Детское</h3>
-				<div id="sc10" class="switchcontent"> Ермолов Алексей +7(909)583-12-10</div>				
+				<div id="sc10" class="switchcontent"> Руководитель Детского служения - Ермолов Алексей +7(909)583-12-10</div>				
 				
 				<h3 onclick="expandcontent('sc11')" style="cursor:hand; cursor:pointer">Вспоможения</h3>
-				<div id="sc11" class="switchcontent"> Вжещ Анатолий (организатор) +7(950)024-96-25 <br> 
-													Оломская Людмила +7(905)224-95-50</div>
+				<div id="sc11" class="switchcontent"> Организатор - Вжещ Анатолий +7(950)024-96-25 <br> 
+													Руководитель служения Вспоможения - Оломская Людмила +7(905)224-95-50</div>
 
 				
 				<!--
